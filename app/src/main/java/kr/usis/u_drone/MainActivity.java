0532c18cca8d51a3081c502e4148a6b3271c00fe
topcoderkz <topcoderkz@gmail.com>
2015-07-14 23:17:20 +0900

    daniel change

--- app/src/main/java/kr/usis/u_drone/MainActivity.java
+++ app/src/main/java/kr/usis/u_drone/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        int daniel = 0;
+        daniel = 5;
 
 
 

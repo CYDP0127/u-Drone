b7c48d14fa54876d0b3daf6807db7d27b54d3af6
CYDP0127 <cydp0127@gmail.com>
2015-07-15 15:42:49 +0900

    connect with usb-serial

--- app/src/main/java/kr/usis/u_drone/MainActivity.java
+++ app/src/main/java/kr/usis/u_drone/MainActivity.java
@@ -1,41 +1,34 @@
 package kr.usis.u_drone;
 
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
 
-import org.mavlink.TestMavlinkReader;
-
+import kr.usis.serial.act.DeviceListActivity;
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView textView;
+    String a = new String();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+    }
+
+    //button event
+    public void onClick(View v){
+        DeviceListActivity dla = new DeviceListActivity(this);
+        dla.getUSBService();
+        dla.refreshDeviceList();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
